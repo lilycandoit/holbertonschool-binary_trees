815c268cbf5b565f0b4415207d54f870a07d0b97
lily <hueduong288@gmail.com>
2025-04-07 12:10:16 +0000

    remove trailing whitespace

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,7 +11,7 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
-	
+
 	/*if no parent, then cen't insert anything */
 	if (parent == NULL)
 		return (NULL);
