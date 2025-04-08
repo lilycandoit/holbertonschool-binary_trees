9b941043a532c708e54de186bb634657e523f476
lily <hueduong288@gmail.com>
2025-04-08 08:34:36 +0000

    measure balance factor -without reusing external file

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -2,6 +2,29 @@
 #include "stdio.h"
 
 /**
+ * calculate_height - Helper function to calculate the height of a subtree
+ * @tree: pointer to the root node of the subtree to measure
+ * Return: height of the tree, accounting for NULL nodes properly
+ */
+static int calculate_height(const binary_tree_t *tree)
+{
+	int left_height, right_height;
+
+	/* Base case: NULL tree has height -1 (for balance calculation) */
+
+	if (tree == NULL)
+		return (-1);
+
+	left_height = calculate_height(tree->left);
+	right_height = calculate_height(tree->right);
+
+	if (left_height > right_height)
+		return (left_height + 1);
+	else
+		return (right_height + 1);
+}
+
+/**
  * binary_tree_balance - to measure the balance factor
  * @tree - pointer to root node of the tree
  * Return: the balance in int or 0 if tree is NULL
@@ -9,16 +32,9 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	size_t left_height = 0, right_height = 0;
-
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left)
-		left_height = binary_tree_height(tree->left) + 1;
-	if (tree->right)
-		right_height = binary_tree_height(tree->right) + 1;
-
-	return (left_height - right_height);
+	return (calculate_height(tree->left) - calculate_height(tree->right));
 }
 
