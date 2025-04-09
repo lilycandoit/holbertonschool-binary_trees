d3819477b0b0c1d2f4d2d687dc739a561c46642c
lily <hueduong288@gmail.com>
2025-04-09 02:01:15 +0000

    find uncle of a node - change condition using grandparent

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,17 +8,17 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	binary_tree_t *parent;
+	binary_tree_t *grandparent;
 
 	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
 
-	parent = node->parent;
+	grandparent = node->parent->parent;
 
-	if (parent->left == node)
-		return (parent->parent->left);
-	if (parent->right == node)
-		return (parent->parent->right);
+	if (grandparent->left == node->parent)
+		return (grandparent->right);
+	if (grandparent->right == node->parent)
+		return (grandparent->left);
 
 	return (NULL);
 }
